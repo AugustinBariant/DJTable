827dc8e7073e96c9efb0e063ebfe649af9d643e7
Nahida Islam <nahida@kth.se>
2019-10-23 12:56:53 +0200

    explosion effect added

--- DJTable_UnityProject/Assets/Scripts/VisualEffectsController.cs
+++ DJTable_UnityProject/Assets/Scripts/VisualEffectsController.cs
@@ -10,12 +10,27 @@ public class VisualEffectsController : MonoBehaviour
 
     public Color[] dialColors = new Color[8];
     public GameObject dialPrefab;
+    public GameObject explosionPrefab;
 
     private Dictionary<int, GameObject> objectPrefabs;
     private Dictionary<int, GameObject> effectInstances;
     private Dictionary<int, GameObject> dialInstances;
 
     private const float halfPI = Mathf.PI / 2f;
+
+    private LineRenderer lineRenderer;
+
+    private readonly int pointsCount = 5;
+    private Vector3[] points;
+
+    private readonly int pointIndexA = 0;
+    private readonly int pointIndexB = 1;
+    private readonly int pointIndexC = 2;
+    private readonly int pointIndexD = 3;
+    private readonly int pointIndexE = 4;
+
+    private float timer;
+    private float timerTimeOut = 0.05f;
 
     // Start is called before the first frame update
     void Start()
@@ -35,14 +50,20 @@ public class VisualEffectsController : MonoBehaviour
         SurfaceInputs.Instance.OnObjectAdd += AddNewObjects;
         SurfaceInputs.Instance.OnObjectUpdate += UpdateObjects;
         SurfaceInputs.Instance.OnObjectRemove += RemoveObjects;
+
+        lineRenderer = GetComponent<LineRenderer>();
+        points = new Vector3[pointsCount];
+        lineRenderer.positionCount = pointsCount;
     }
 
     void AddNewObjects(List<ObjectInput> addedObjects)
     {
+
         foreach (ObjectInput addedObject in addedObjects)
         {
             int tagValue = addedObject.tagValue;
             GameObject prefab;
+
             if (objectPrefabs.TryGetValue(tagValue, out prefab) && !effectInstances.ContainsKey(tagValue))
             {
                 GameObject instance = Instantiate(prefab, addedObject.position, Quaternion.identity);
@@ -71,7 +92,49 @@ public class VisualEffectsController : MonoBehaviour
 
                 dialInstances.Add(tagValue, instance);
             }
+
+            //Adds the explosion prefab
+            if (Vector3.Distance(transform.position, addedObject.position) < 10)
+            {
+                GameObject instance = Instantiate(explosionPrefab, addedObject.position, Quaternion.identity);
+                effectInstances.Add(tagValue, instance);
+            }
+
+
+
+            //timer += Time.deltaTime;
+
+            //if (timer > timerTimeOut)
+            //{
+            //    timer = 0;
+
+            //    points[pointIndexA] = transform.position;
+            //    points[pointIndexE] = addedObject.position;
+            //    points[pointIndexC] = GetCenter(points[pointIndexA], points[pointIndexE]);
+            //    points[pointIndexB] = GetCenter(points[pointIndexA], points[pointIndexC]);
+            //    points[pointIndexD] = GetCenter(points[pointIndexC], points[pointIndexE]);
+
+            //    //distance = Vector3.Distance(start.position, end.position) / points.Length;
+
+            //    if ((Vector3.Distance(transform.position, addedObject.position) / points.Length) < 2)
+            //    {
+            //        lineRenderer.SetPositions(points);
+            //        lineRenderer.SetVertexCount(5);
+            //        Debug.Log("THE LINE!!!!!");
+            //    }
+            //    else
+            //    {
+            //        //Destroy(lineRenderer.gameObject, 5);
+            //        lineRenderer.SetVertexCount(0);
+            //        Debug.Log("DESTROYED");
+
+            //    }
+            //}
         }
+    }
+    private Vector3 GetCenter(Vector3 a, Vector3 b)
+    {
+        return (a + b) / 2;
     }
 
     void UpdateObjects(List<ObjectInput> updatedObjects)
@@ -117,3 +180,4 @@ public class VisualEffectsController : MonoBehaviour
     }
 
 }
+
