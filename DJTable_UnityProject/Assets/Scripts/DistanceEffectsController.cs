be889da857870f7fbd69ce0f49ea78c1e8b6dd3e
Nahida Islam <nahida@kth.se>
2019-10-26 22:48:51 +0200

    updates with addedobject, updateobject and removedobject

--- DJTable_UnityProject/Assets/Scripts/DistanceEffectsController.cs
+++ DJTable_UnityProject/Assets/Scripts/DistanceEffectsController.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class DistanceEffectsController : MonoBehaviour
 {
     public GameObject expoldingPrefab;
+    private Dictionary<int, GameObject> distanceInstances;
 
 
     void Start()
     {
-        SurfaceInputs.Instance.OnObjectAdd += DistanceEffect;
+
+        SurfaceInputs.Instance.OnObjectAdd += AddNewObjects;
+        SurfaceInputs.Instance.OnObjectUpdate += UpdateObjects;
+        SurfaceInputs.Instance.OnObjectRemove += RemoveObjects;
     }
 
-    void DistanceEffect(List<ObjectInput> addedObjects)
+    void AddNewObjects(List<ObjectInput> addedObjects)
     {
         foreach (ObjectInput addedObject in addedObjects)
         {
+            int tagValue = addedObject.tagValue;
+
             foreach (KeyValuePair<int, ObjectInput> otherObject in SurfaceInputs.Instance.surfaceObjects)
             {
                 var nearestDist = 2.0;
@@ -26,17 +32,43 @@ public class DistanceEffectsController : MonoBehaviour
                 {
                     if (distance < nearestDist)
                     {
+                        Debug.Log("Distance based effect on AddedObject");
                         nearestDist = distance;
                         nearestObj = otherObject.Value;
-                        Debug.Log("Distance based effect");
+        
                         GameObject instance = Instantiate(expoldingPrefab, GetCenter(addedObject.position, nearestObj.position), Quaternion.identity);
-                        //distEffectInstances.Add(tagValue, instance);
+                        distanceInstances.Add(tagValue, instance);
+                       
                     }
-                    //Debug.Log("okay");
-                    //Debug.DrawLine(addedObject.position, nearestObj.position, Color.red);
                 }
                 else
                     continue;
+            }
+        }
+    }
+    void UpdateObjects(List<ObjectInput> updatedObjects)
+    {
+        foreach (ObjectInput updatedObject in updatedObjects)
+        {
+            GameObject instance;
+            if (distanceInstances.TryGetValue(updatedObject.tagValue, out instance))
+            {
+                instance.transform.localPosition = updatedObject.position;
+                Debug.Log("Distance based effect on UpdateObject");
+                //instance.transform.localRotation = Quaternion.Euler(0, 0, -entry.Value.orientation * Mathf.Rad2Deg);
+            }
+        }
+    }
+    void RemoveObjects(List<ObjectInput> removedObjects)
+    {
+        foreach (ObjectInput removedObject in removedObjects)
+        {
+            GameObject instance;
+            if (distanceInstances.TryGetValue(removedObject.tagValue, out instance))
+            {
+                Destroy(instance);
+                distanceInstances.Remove(removedObject.tagValue);
+                Debug.Log("Distance based effect on RemovedObject");
             }
         }
     }
