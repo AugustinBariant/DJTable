1c583636e7840629770d36a53fbdb5e480c298e3
Nahida Islam <nahida@kth.se>
2019-10-29 22:44:16 +0100

    updated addedobject function

--- DJTable_UnityProject/Assets/Scripts/DistanceEffect/DistanceEffectsController.cs
+++ DJTable_UnityProject/Assets/Scripts/DistanceEffect/DistanceEffectsController.cs
@@ -5,83 +5,108 @@ using UnityEngine;
 public class DistanceEffectsController : MonoBehaviour
 {
     public GameObject expoldingPrefab;
-    private Dictionary<int, GameObject> distanceInstances;
-    private Dictionary<ObjectGroup, GameObject> effectInstances;
 
+    private List<ObjectInput> singleObjects;
     private List<ObjectGroup> groupList;
+
+    private bool grouped = false;
+    const float nearestDist = 3f;
 
     void Start()
     {
-        distanceInstances = new Dictionary<int, GameObject>();
-        effectInstances = new Dictionary<ObjectGroup, GameObject>();
 
         SurfaceInputs.Instance.OnObjectAdd += AddNewObjects;
         SurfaceInputs.Instance.OnObjectUpdate += UpdateObjects;
         SurfaceInputs.Instance.OnObjectRemove += RemoveObjects;
+
+        singleObjects = new List<ObjectInput>(SurfaceInputs.Instance.surfaceObjects.Values);
     }
 
     void AddNewObjects(List<ObjectInput> addedObjects)
     {
-        List<ObjectInput> otherObjects = new List<ObjectInput>(SurfaceInputs.Instance.surfaceObjects.Values);
-        
-  
         foreach (ObjectInput addedObject in addedObjects)
         {
-            foreach (ObjectInput otherObject in otherObjects)
+            //go through each group in the group list
+            foreach (ObjectGroup group in groupList)
             {
-                var nearestDist = 2.0;
+                //check the distance between the new object and the existing groups center
+                float distance = Vector3.Distance(addedObject.position, group.groupCenter);
+                if (distance < nearestDist)
+                {
+                    // add addedObject to group's object list
+                    group.addObject(addedObject);
+                    // recalculate and update group center point
+                    Vector2 center = GetCenter(group);
+                    // update position and size of the effect instance
+                    GameObject instance = Instantiate(expoldingPrefab, center, Quaternion.identity);
+
+                    //ObjectGroup objectGroup = new ObjectGroup(instance, objects, center);
+                    //groupList.Add(objectGroup);
+
+                    grouped = true;
+                    break;
+                }
+            }
+
+            if (grouped == true)
+            {
+                continue;
+            }
+            //Instantiate an effect on a group of 2 single objects
+            foreach (ObjectInput otherObject in singleObjects)
+            {
                 float distance = Vector3.Distance(addedObject.position, otherObject.position);
 
                 if (addedObject.tagValue != otherObject.tagValue)
                 {
-                    Debug.Log("1st if-statement");
                     if (distance < nearestDist)
                     {
-                        Debug.Log("2nd if-statement");
-                        ObjectGroup currentGroup = checkGroup(addedObject, otherObject);
-                        Debug.Log("current group");
-                        GameObject instance = Instantiate(expoldingPrefab, GetCenter(currentGroup), Quaternion.identity);
+                        Vector2 center = (addedObject.position + otherObject.position) / 2;
+                        GameObject instance = Instantiate(expoldingPrefab, center, Quaternion.identity);
 
-                        //GameObject instance = Instantiate(expoldingPrefab, GetCenter(addedObject.position, otherObject.position), Quaternion.identity);
-                        Debug.Log("Object:" + addedObject.id + addedObject.position);
+                        List<ObjectInput> objects = new List<ObjectInput>();
+                        objects.Add(addedObject);
+                        objects.Add(otherObject);
+                        ObjectGroup objectGroup = new ObjectGroup(instance, objects, center);
+                        groupList.Add(objectGroup);
 
+                        singleObjects.Remove(otherObject);
 
-                        //distanceInstances.Add(addedObject.tagValue, instance);
-                        effectInstances.Add(currentGroup, instance);
+                        grouped = true;
+                        break;
                     }
-                    else
-                        Destroy(gameObject);
                 }
             }
+
+            if (grouped == true)
+            {
+                continue;
+            }
+            //add to the single object list if none of the statement matches
+            singleObjects.Add(addedObject);
         }
     }
 
-    ObjectGroup checkGroup(ObjectInput addedObject, ObjectInput otherObject)
+    void UpdateObjects(List<ObjectInput> updatedObjects)
     {
-        //for each group in the group list
-        foreach (ObjectGroup currentGroup in groupList)
+        foreach (ObjectInput updatedObject in updatedObjects)
         {
-            //for each object in the group
-            foreach (ObjectInput currentObject in currentGroup.objectList)
-            {
-                //if other object exists
-                if(otherObject.id == currentObject.id)
-                {
-                    //add the new object to the group
-                    currentGroup.addObject(addedObject);
+            //    instance.transform.localPosition = updatedObject.position;
 
-                    return currentGroup;
-                }
-            }
         }
-        //otherwise create a new group for them
-        ObjectGroup newGroup = new ObjectGroup();
-        newGroup.addObject(addedObject);
-        newGroup.addObject(otherObject);
-        groupList.Add(newGroup);
-   
-        return newGroup;
-
+    }
+    void RemoveObjects(List<ObjectInput> removedObjects)
+    {
+        foreach (ObjectInput removedObject in removedObjects)
+        {
+            GameObject instance;
+            //if (distanceInstances.TryGetValue(removedObject.tagValue, out instance))
+            //{
+            //    Destroy(instance);
+            //    distanceInstances.Remove(removedObject.tagValue);
+            //    Debug.Log("Distance based effect on RemovedObject");
+            //}
+        }
     }
 
     private Vector2 GetCenter(ObjectGroup objectGroup)
@@ -98,41 +123,38 @@ public class DistanceEffectsController : MonoBehaviour
         var centerY = sumY / objectGroup.objectList.Count;
         Vector2 center = new Vector2(centerX, centerY);
         return center;
-    }
+    } 
 
-
-    void UpdateObjects(List<ObjectInput> updatedObjects)
-    {
-        foreach (ObjectInput updatedObject in updatedObjects)
-        {
-            GameObject instance;
-            if (distanceInstances.TryGetValue(updatedObject.tagValue, out instance))
-            {
-                instance.transform.localPosition = updatedObject.position;
-                Debug.Log("Distance based effect on UpdateObject");
-                //instance.transform.localRotation = Quaternion.Euler(0, 0, -entry.Value.orientation * Mathf.Rad2Deg);
-            }
-        }
-    }
-    void RemoveObjects(List<ObjectInput> removedObjects)
-    {
-        foreach (ObjectInput removedObject in removedObjects)
-        {
-            GameObject instance;
-            if (distanceInstances.TryGetValue(removedObject.tagValue, out instance))
-            {
-                Destroy(instance);
-                distanceInstances.Remove(removedObject.tagValue);
-                Debug.Log("Distance based effect on RemovedObject");
-            }
-        }
-    }
-
-    //private Vector3 GetCenter(Vector3 a, Vector3 b)
+    //ObjectGroup checkGroup(ObjectInput addedObject, ObjectInput otherObject)
     //{
-    //    return (a + b) / 2;
+
+    //    //for each group in the group list
+    //    foreach (ObjectGroup currentGroup in groupList)
+    //    {
+    //        //for each object in the group
+    //        foreach (ObjectInput currentObject in currentGroup.objectList)
+    //        {
+    //            //if other object exists
+    //            if(otherObject.tagValue == currentObject.tagValue)
+    //            {
+    //                //add the new object to the group
+    //                currentGroup.addObject(addedObject);
+
+    //                return currentGroup;
+    //            }
+    //        }
+    //    }
+    //    //otherwise create a new group for them
+    //    ObjectGroup newGroup = new ObjectGroup();
+    //    newGroup.addObject(addedObject);
+    //    newGroup.addObject(otherObject);
+
+    //    groupList.Add(newGroup);
+
+    //    return newGroup;
 
     //}
+
 
 }
 
@@ -140,25 +162,27 @@ class ObjectGroup
 {
     GameObject effectInstance;
     public List<ObjectInput> objectList;
+    public Vector2 groupCenter;
     private Dictionary<ObjectInput, GameObject> distanceInstances;
+
+    //Constructor
+    public ObjectGroup(GameObject effectInstane, List<ObjectInput> objectList, Vector2 center)
+    {
+        this.effectInstance = effectInstance;
+        this.objectList = objectList;
+        this.groupCenter = center;
+    }
 
     //Add an object into the list
     public void addObject(ObjectInput id)
     {
-       if(distanceInstances.TryGetValue(id, out effectInstance)){
-            objectList.Add(id);
-        }
-        //objectList.Add(id);
+        objectList.Add(id);
 
     }
     //Remove an object from the list
     public void removeObject(ObjectInput id)
     {
-        //objectList.Remove(id);
-        if (distanceInstances.TryGetValue(id, out effectInstance))
-        {
-            objectList.Remove(id);
-        }
+        objectList.Remove(id);
     }
 
 }
